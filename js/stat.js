02aee64100d0694bf38869a507f9b2e8bc4b592b
Tim <32385143+arcticstormx@users.noreply.github.com>
2020-06-04 20:22:21 +0300

    renderStatistics is done! Finally!

--- js/stat.js
+++ js/stat.js
@@ -10,20 +10,33 @@ var BAR_WIDTH = 40;
 var BAR_MAX_HEIGHT = 150;
 var FONT_GAP = 10;
 var FONT_HEIGHT = 16;
-var maxScore
 
 var renderCloud = function(ctx, x, y, color) {
   ctx.fillStyle = color;
   ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
 };
 
-var renderPlayer = function(ctx, playerIndex, playerName, playerScore, playerColor) {
-  // if (playerName === "Вы") playerColor = "#000";
-  ctx.fillStyle = "red";
-  var barHeight = playerScore / maxScore;
-  ctx.fillText(playerName, CLOUD_X + GAP + (BAR_WIDTH + BAR_GAP) * playerIndex, CLOUD_Y + CLOUD_HEIGHT - BAR_GAP + FONT_GAP);
-  ctx.fillText(playerScore, CLOUD_X + GAP + (BAR_WIDTH + BAR_GAP) * playerIndex, CLOUD_Y + CLOUD_HEIGHT - BAR_GAP + FONT_GAP);
+var getMaxElement = function(arr) {
+  var maxElement = arr[0];
+
+  for (var i = 1; i < arr.length; i++) {
+    if (arr[i] > maxElement) {
+      maxElement = arr[i];
+    }
+  }
+
+  return maxElement;
+};
+
+var renderPlayer = function(ctx, playerIndex, playerName, playerScore, maxScore) {
+  var barHeight = playerScore / maxScore * BAR_MAX_HEIGHT;
+  var playerColor;
+  playerName === "Вы" ? playerColor = "rgba(255, 0, 0, 1)" : playerColor = "hsl(240, " + randomNumber(0, 100) + "%, 50%)";
+  ctx.fillStyle = playerColor;
   ctx.fillRect(CLOUD_X + GAP + (BAR_WIDTH + BAR_GAP) * playerIndex, CLOUD_Y + CLOUD_HEIGHT - BAR_GAP, BAR_WIDTH, -barHeight);
+  ctx.fillStyle = "#000";
+  ctx.fillText(playerName, CLOUD_X + GAP + (BAR_WIDTH + BAR_GAP) * playerIndex, CLOUD_Y + CLOUD_HEIGHT - BAR_GAP + FONT_GAP);
+  ctx.fillText(playerScore, CLOUD_X + GAP + (BAR_WIDTH + BAR_GAP) * playerIndex, CLOUD_Y + CLOUD_HEIGHT - BAR_GAP - barHeight - FONT_HEIGHT);
 };
 
 var randomNumber = function(min, max) {
@@ -34,14 +47,14 @@ window.renderStatistics = function(ctx, namesArr, timesArr) {
     renderCloud(ctx, CLOUD_X + 10, CLOUD_Y + 10, 'rgba(0, 0, 0, 0.7)');
     renderCloud(ctx, CLOUD_X, CLOUD_Y, '#fff');
 
+    var maxTime = getMaxElement(timesArr);
+
     ctx.fillStyle = '#000';
     ctx.textBaseline = 'top';
     ctx.font = "16px PT Mono";
     ctx.fillText("Ура вы победили!", CLOUD_X + GAP / 2, CLOUD_Y + GAP / 2);
     ctx.fillText("Список результатов:", CLOUD_X + GAP / 2, CLOUD_Y + GAP / 2 + FONT_HEIGHT);
-    renderPlayer(ctx, 0, namesArr[0])
-    // for (let i = 0; i < namesArr.length; i++) {
-    //   renderPlayer(i, namesArr[i], timesArr[i], null);
-    //   // renderPlayer(i, namesArr[i], timesArr[i], "hsl(240, " + randomNumber(0, 100) + "%, 50%)");
-    // }
+    for (var i = 0; i < namesArr.length; i++) {
+      renderPlayer(ctx, i, namesArr[i], Math.round(timesArr[i]), maxTime);
+    }
 };
